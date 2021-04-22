fb216008a43f6aa1c205b3fe7787fd8ba366d78a
wmc41 <wmc41@scarletmail.rutgers.edu>
2021-04-22 17:07:46 -0400

    Audio Mixer + Snapshots

--- Assets/Player_Audio.cs
+++ Assets/Player_Audio.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Player_Audio : MonoBehaviour
 {
@@ -8,11 +9,18 @@ public class Player_Audio : MonoBehaviour
 
     public AudioSource audioS;
 
+    public AudioMixerSnapshot idleSnapshot;
+    public AudioMixerSnapshot auxinSnapshot;
+
 private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Water"))
         {
             audioS.PlayOneShot(splashSound);
+        }
+        if (other.CompareTag("Enemy_Zone"))
+        {
+            auxinSnapshot.TransitionTo(0.5f);
         }
     }
 private void OnTriggerExit(Collider other)
@@ -20,6 +28,10 @@ private void OnTriggerExit(Collider other)
         if (other.CompareTag("Water"))
         {
             audioS.PlayOneShot(splashSound);
+        }
+        if (other.CompareTag("Enemy_Zone"))
+        {
+            idleSnapshot.TransitionTo(0.5f);
         }
     }
 }
